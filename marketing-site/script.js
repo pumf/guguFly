7dd6902bdb115e1cf0c31229b835d2f7b5e43022
pumengfei <517858721@qq.com>
2026-06-05 15:07:45 +0800

    chore: remove marketing content from repo

--- marketing-site/script.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll('.nav-link');
-
-for (const link of links) {
-  link.addEventListener('click', () => {
-    document.querySelectorAll('.nav-link').forEach(item => item.classList.remove('is-active'));
-    link.classList.add('is-active');
-  });
-}
